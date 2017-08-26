313127ac038351b95df9c723052ccd73cc184a2a
Robert Oswald <robert@rocketpixelstudios.com>
2017-08-26 17:09:29 +0200

    Add eth pricing tracker

---
+++ eth-pricing.php
@@ -0,0 +1,19 @@
+<?php
+	$url = 'https://coinmarketcap-nexuist.rhcloud.com/api/eth';
+
+	$buf = json_decode(file_get_contents($url));
+
+	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+	$server = $url["host"];
+	$username = $url["user"];
+	$password = $url["pass"];
+	$db = substr($url["path"], 1);
+
+	$conn = new mysqli($server, $username, $password, $db);
+
+	$sql = 'INSERT INTO pricing (`buffer`, `created_at`) VALUES ("' . json_encode($buf) . '", now())';
+
+	$conn->query($sql);
+
+	$conn->close()
